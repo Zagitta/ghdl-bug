bb51f29d558d700a32da38b315d2ef77a0769386
Simon Rasmussen <zetlon@gmail.com>
2018-03-07 22:22:20 +0100

    Minimal reproduction

---
+++ top.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.pkg1.all;
+use work.pkg2.all;
+
+entity top is
+port (
+    clk : in std_logic
+);
+end top;
+
+architecture arch of top is
+begin
+end architecture;
